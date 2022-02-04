9eaa05a4fc014f266e013c388ee163c5dd2d5c50
Eduard Suslov <OvanessTea@yandex.com>
2022-02-05 02:07:34 +0300

    adding search-bar styles and dates

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
 import { API_KEY, API_URL } from "./config";
 import React, { useState } from "react";
+import Clock from "react-live-clock";
 
 function App() {
     const { search, setSearch } = useState("");
     const { weather, setWeather } = useState({});
+
+    const dateBuilder = (dateInfo) => {
+        let months = [
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December",
+        ];
+        let days = [
+            "Sunday",
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday",
+        ];
+
+        let day = days[dateInfo.getDay()];
+        let date = dateInfo.getDate();
+        let month = months[dateInfo.getMonth()];
+        let year = dateInfo.getFullYear();
+
+        return `${day} ${date} ${month} ${year}`;
+    };
 
     const pressKeyDown = (e) => {
         if (e.key === "Enter") {
@@ -19,16 +53,32 @@ function App() {
 
     return (
         <div className="app">
-            <header className="header__search">
-                <input
-                    type="text"
-                    className="search"
-                    placeholder="Search..."
-                    value={search}
-                    onChange={(e) => setSearch(e.target.value)}
-                    onKeyDown={(e) => pressKeyDown(e)}
-                />
-            </header>
+            <main className="main">
+                <div className="search-box">
+                    <input
+                        type="text"
+                        className="search-bar"
+                        placeholder="Search..."
+                        value={search}
+                        onChange={(e) => setSearch(e.target.value)}
+                        onKeyDown={(e) => pressKeyDown(e)}
+                    />
+                    <Clock
+                        format={"HH:mm"}
+                        className="clocks"
+                        ticking={true}
+                        timezone={"Europe/Moscow"}
+                    />
+                </div>
+                <div className="location-box">
+                    <div className="location">New York City, US</div>
+                    <div className="date">{dateBuilder(new Date())}</div>
+                </div>
+                <div className="weather-box">
+                    <div className="temp">15&#176;c</div>
+                    <div className="weather">Sunny</div>
+                </div>
+            </main>
         </div>
     );
 }

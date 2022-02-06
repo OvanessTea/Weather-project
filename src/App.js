5c1cabeb72d78dc6715d90779ab40d04c1379fea
Eduard Suslov <OvanessTea@yandex.com>
2022-02-07 00:52:09 +0300

    publish

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { API_KEY, API_URL } from "./config";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Clock from "react-live-clock";
 
 function App() {
-    const { search, setSearch } = useState("");
-    const { weather, setWeather } = useState({});
+    const [search, setSearch] = useState("");
+    const [weather, setWeather] = useState({});
+    const [backgroundImage, setBackgroundImage] = useState("app");
 
     const dateBuilder = (dateInfo) => {
         let months = [
@@ -40,19 +41,34 @@ function App() {
     };
 
     const pressKeyDown = (e) => {
-        if (e.key === "Enter") {
+        if (e.key === "Enter" && search !== "") {
             fetch(`${API_URL}weather?q=${search}&units=metric&APPID=${API_KEY}`)
                 .then((response) => response.json())
                 .then((result) => {
                     setWeather(result);
                     setSearch("");
-                    console.log(result);
+                    switch (result.weather[0].main) {
+                        case "Clouds":
+                            setBackgroundImage("app clouds");
+                            break;
+                        case "Snow":
+                            setBackgroundImage("app winter");
+                            break;
+                        case "Clear":
+                            setBackgroundImage("app clear");
+                            break;
+                        case "Smoke":
+                            setBackgroundImage("app smoke");
+                            break;
+                        default:
+                            setBackgroundImage("app");
+                    }
                 });
         }
     };
 
     return (
-        <div className="app">
+        <div className={backgroundImage}>
             <main className="main">
                 <div className="search-box">
                     <input
@@ -70,14 +86,28 @@ function App() {
                         timezone={"Europe/Moscow"}
                     />
                 </div>
-                <div className="location-box">
-                    <div className="location">New York City, US</div>
-                    <div className="date">{dateBuilder(new Date())}</div>
-                </div>
-                <div className="weather-box">
-                    <div className="temp">15&#176;c</div>
-                    <div className="weather">Sunny</div>
-                </div>
+                {typeof weather.main !== "undefined" ? (
+                    <>
+                        <div className="location-box">
+                            <div className="location">
+                                {weather.name}, {weather.sys.country}
+                            </div>
+                            <div className="date">
+                                {dateBuilder(new Date())}
+                            </div>
+                        </div>
+                        <div className="weather-box">
+                            <div className="temp">
+                                {Math.round(weather.main.temp)}&#176;c
+                            </div>
+                            <div className="weather">
+                                {weather.weather[0].main}
+                            </div>
+                        </div>
+                    </>
+                ) : (
+                    ""
+                )}
             </main>
         </div>
     );
